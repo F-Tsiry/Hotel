f0be987f8d62e962b6bfa41e0d26f9b697cd97ed
F-Tsiry <fanomezantsoatsiry36@gmail.com>
2026-06-23 15:49:27 +0300

    Update Chambre.java

--- main/java/HotelExo/Chambre.java
+++ main/java/HotelExo/Chambre.java
@@ -14,11 +14,24 @@ public class Chambre {
     }
 
     // Getters
-    public int getNumero() { return numero; }
-    public int getNombreLits() { return nombreLits; }
-    public double getPrixParNuit() { return prixParNuit; }
+    @Getter
+    private int numero;
+    @Getter
+    private int nombreLits;
+    @Getter
+    private double prixParNuit;
+    //setter
+    @Setter
+    private boolean disponible;
+
+    public Chambre(int numero, int nombreLits, double prixParNuit) {
+        this.numero = numero;
+        this.nombreLits = nombreLits;
+        this.prixParNuit = prixParNuit;
+        this.disponible = true;
+    }
+
     public boolean estDisponible() { return disponible; }
-    public void setDisponible(boolean disponible) { this.disponible = disponible; }
 
     @Override
     public String toString() {
