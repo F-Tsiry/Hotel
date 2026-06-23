235c6723033111941a9336940d0cd2a7a110b3be
F-Tsiry <fanomezantsoatsiry36@gmail.com>
2026-06-23 15:50:40 +0300

    Refactor Chambre class by removing duplicates

--- main/java/HotelExo/Chambre.java
+++ main/java/HotelExo/Chambre.java
@@ -1,26 +1,14 @@
 package HotelExo;
 
 public class Chambre {
-    private int numero;
-    private int nombreLits;
-    private double prixParNuit;
-    private boolean disponible;
 
-    public Chambre(int numero, int nombreLits, double prixParNuit) {
-        this.numero = numero;
-        this.nombreLits = nombreLits;
-        this.prixParNuit = prixParNuit;
-        this.disponible = true;
-    }
-
-    // Getters
     @Getter
     private int numero;
     @Getter
     private int nombreLits;
     @Getter
     private double prixParNuit;
-    //setter
+    
     @Setter
     private boolean disponible;
 
